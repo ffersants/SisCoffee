48053931d6c30c15af4022a6357dfe54f21725f1
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2021-02-18 11:51:10 -0300

    Resolvido o bug da tela em modo retrato.

---
+++ frontend/Vue/index-page/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <HeaderDefault>
+      <i id="user-config" class="fas fa-users-cog"></i>
+      <i id="list-users" class="fas fa-list"></i>
+    </HeaderDefault>
+    <list-users-index></list-users-index>
+  </div>
+</template>
+
+<script>
+import ListUsersIndex from './components/ListUsersIndex.vue'
+import HeaderDefault from './components/HeaderDefault.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderDefault,
+    ListUsersIndex
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/Vue/index-page/src/components/HeaderDefault.vue
@@ -0,0 +1,62 @@
+<template>
+  <b-container class="mt-4">
+    <b-row>
+      <b-col class="text-left">
+        <slot id="slot"></slot>
+      </b-col>
+      <b-col class="text-right">
+        <span>{{time}}</span>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+  export default {
+    name: "HeaderDefault",
+    data() {
+      return {
+        time: ""
+      }
+    },
+    methods: {
+
+    },
+    beforeCreate(){
+    setInterval(() => {
+            let date = new Date().toLocaleDateString("pt-br", {
+                dateStyle: 'short'
+            });
+            let weekday = new Date().toLocaleDateString("pt-br", {
+                weekday: 'short'
+            });
+            let time = new Date().toLocaleTimeString("pt-br", {
+                timeStyle: 'short',
+                hour12: false
+            });
+            
+            weekday = weekday[0].toUpperCase() + weekday.substr(1, 2) 
+            
+            this.time = `${date} ${weekday} ${time}`
+        }, 1000);
+  }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  span{
+    color: #F1E5D1;
+  }
+
+  i{
+    font-size: 2em;
+    color: #F1E5D1;
+  }
+  i:nth-child(2){
+    margin-left: 0.8em;
+  }
+  i:hover{
+    cursor: pointer;
+  }
+</style>
