1a093440a9b885a8171a6a303f7d0bf28d371f83
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2021-02-28 05:17:16 -0300

    Admpswd hasheada no backend também.

--- backend/src/database/migrations/20210207212050_adm.js
+++ backend/src/database/migrations/20210207212050_adm.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('adm_tb', function (table) {
         table.string('userName').defaultTo('coffeeadm');
-        table.string('password').defaultTo('4fe879f30bca10fdd4b8cd12288d788b');
+        table.string('password').defaultTo('0f3aa814208a1e17417fd762dcb599f5');
     })
 };
 
