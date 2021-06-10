abe3e267803e5dc77f1197b675d524215de15736
Fernando Ferreira - HPZ4 <fersants.nando.ferreira@gmail.com>
2021-06-10 18:33:22 -0300

    Email do usuário passado para o modal de exclusão do usuário.

--- frontend/Vue/index-page/src/components/SuccessAlert.vue
+++ frontend/Vue/index-page/src/components/SuccessAlert.vue
@@ -1,52 +1,57 @@
 <template>
-  <modal-base>
-    <div id="alert-success">
-      <h1>AÇÃO REALIZADA</h1>
-      <!-- icon -->
-      <slot></slot>
+	<modal-base>
+		<div id="alert-success">
+			<h1>AÇÃO REALIZADA</h1>
+			<!-- icon -->
+			<slot></slot>
 
-      <b-container>
-        <b-row id="first-row" class="text-center">
-          <b-col cols="5">
-            <p>Nome</p>
-            <!-- <p>Fernando Santos</p> -->
-            <p>{{ userInAction.name }}</p>
-          </b-col>
+			<b-container>
+				<b-row id="first-row" class="text-center">
+					<b-col cols="5">
+						<p>Nome</p>
+						<!-- <p>Fernando Santos</p> -->
+						<p>{{ userInAction.name }}</p>
+					</b-col>
 
-          <b-col cols="2">
-            <p>Seção</p>
-            <!-- <p>SSTM</p> -->
-            <p>{{ userInAction.section }}</p>
-          </b-col>
+					<b-col cols="2">
+						<p>Seção</p>
+						<!-- <p>SSTM</p> -->
+						<p>{{ userInAction.section }}</p>
+					</b-col>
 
-          <b-col cols="5">
-            <div v-if="action == 'payment'">
-              <p>Data do pagamento</p>
-              <!-- <p>28/08/2000</p> -->
-              <p>{{ userInAction.currentDate }}</p>
-            </div>
+					<b-col cols="5">
+						<div v-if="action == 'payment'">
+							<p>Data do pagamento</p>
+							<!-- <p>28/08/2000</p> -->
+							<p>{{ userInAction.currentDate }}</p>
+						</div>
 
-            <div v-else-if="action == 'signUp'">
-              <p>Data do cadastro</p>
-              <!-- <p>28/08/2000</p> -->
-              <p>{{ userInAction.currentDate }}</p>
-            </div>
+						<div v-else-if="action == 'signUp'">
+							<p>Data do cadastro</p>
+							<!-- <p>28/08/2000</p> -->
+							<p>{{ userInAction.currentDate }}</p>
+						</div>
 
-            <div v-else>
-              <p>Data do cadastro</p>
-              <!-- <p>28/08/2000</p> -->
-              <p>{{ userInAction.signUpDate }}</p>
-            </div>
-          </b-col>
-        </b-row>
-      </b-container>
+						<div v-else>
+							<p>Data do cadastro</p>
+							<!-- <p>28/08/2000</p> -->
+							<p>{{ userInAction.signUpDate }}</p>
+						</div>
+					</b-col>
 
-      <div id="main">
-        <p>{{ description }}</p>
-        <button @click="closeSuccessAlert()">OK</button>
-      </div>
-    </div>
-  </modal-base>
+					<b-col class="text-center">
+						<p>E-mail</p>
+						<p>{{ userInAction.email }}</p>
+					</b-col>
+				</b-row>
+			</b-container>
+
+			<div id="main">
+				<p>{{ description }}</p>
+				<button @click="closeSuccessAlert()">OK</button>
+			</div>
+		</div>
+	</modal-base>
 </template>
 
 <script>
@@ -56,86 +61,86 @@ import { EventBus } from "../event-bus.js";
 import ModalBase from "./ModalBase.vue";
 
 export default {
-  name: "SuccessAlert",
-  props: {
-    action: {
-      type: String,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-  },
-  computed: mapState(["userInAction"]),
-  methods: {
-    closeSuccessAlert() {
-      this.$destroy();
-      EventBus.$emit("closeSuccessAlert");
-    },
-  },
-  components: {
-    ModalBase,
-  },
+	name: "SuccessAlert",
+	props: {
+		action: {
+			type: String,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+	},
+	computed: mapState(["userInAction"]),
+	methods: {
+		closeSuccessAlert() {
+			this.$destroy();
+			EventBus.$emit("closeSuccessAlert");
+		},
+	},
+	components: {
+		ModalBase,
+	},
 };
 </script>
 
 <style scoped>
 #alert-success {
-  padding: 1em;
-  width: 60vw !important;
+	padding: 1em;
+	width: 60vw !important;
 }
 
 #alert-success h1 {
-  font-size: 3em;
+	font-size: 3em;
 }
 
 p {
-  margin: 0 !important;
-  padding: 0 !important;
-  text-align: center;
-  font-size: 1.5em !important;
+	margin: 0 !important;
+	padding: 0 !important;
+	text-align: center;
+	font-size: 1.5em !important;
 }
 
 p:nth-child(2) {
-  font-size: 1.5em !important;
+	font-size: 1.5em !important;
 }
 
 i {
-  color: #f0a82e !important;
+	color: #f0a82e !important;
 }
 
 #main {
-  margin-top: 5%;
+	margin-top: 5%;
 }
 
 #main p {
-  font-size: 1.8em !important;
-  margin-bottom: 5% !important;
+	font-size: 1.8em !important;
+	margin-bottom: 5% !important;
 }
 
 #main button:active {
-  background-color: #fcab1f;
+	background-color: #fcab1f;
 }
 
 @media (max-width: 575.98px) {
-  #alert-success {
-    padding: 1em -1em;
-    width: 100vw;
-    font-size: 0.7rem;
-  }
+	#alert-success {
+		padding: 1em -1em;
+		width: 100vw;
+		font-size: 0.7rem;
+	}
 }
 
 @media (min-width: 576px) and (max-width: 767.98px) {
-  #alert-success {
-    width: 90vw;
-  }
+	#alert-success {
+		width: 90vw;
+	}
 }
 
 @media (min-width: 768px) and (max-width: 991.98px) {
-  #alert-success {
-    width: 90vw;
-    font-size: 0.8rem;
-  }
+	#alert-success {
+		width: 90vw;
+		font-size: 0.8rem;
+	}
 }
 
 @media (min-width: 992px) and (max-width: 1199.98px) {
--- frontend/Vue/index-page/src/views/RemoveUser.vue
+++ frontend/Vue/index-page/src/views/RemoveUser.vue
@@ -1,75 +1,75 @@
 <template>
-  <transition name="remove-user">
-    <div id="remove-users">
-      <HeaderDefault title="REMOVER USUÁRIO">
-        <router-link to="/">
-          <i class="fas fa-arrow-left"></i>
-        </router-link>
-      </HeaderDefault>
+	<transition name="remove-user">
+		<div id="remove-users">
+			<HeaderDefault title="REMOVER USUÁRIO">
+				<router-link to="/">
+					<i class="fas fa-arrow-left"></i>
+				</router-link>
+			</HeaderDefault>
 
-      <table-with-users :action="action"> </table-with-users>
+			<table-with-users :action="action"> </table-with-users>
 
-      <div id="modal-area">
-        <transition>
-          <success-alert
-            description="Usuário removido com sucesso!"
-            v-if="showSuccessAlert"
-          >
-            <div class="text-center mb-2 mt-2" id="icon-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="55"
-                height="40"
-                viewBox="0 0 74.227 56.933"
-              >
-                <path
-                  id="user-times-solid"
-                  d="M68.39,26.687l5.289-5.071a1.74,1.74,0,0,0,0-2.535l-2.645-2.535a1.927,1.927,0,0,0-2.645,0L63.1,21.617l-5.289-5.071a1.927,1.927,0,0,0-2.645,0l-2.645,2.535a1.74,1.74,0,0,0,0,2.535l5.289,5.071-5.289,5.071a1.74,1.74,0,0,0,0,2.535l2.645,2.535a1.927,1.927,0,0,0,2.645,0L63.1,31.758l5.289,5.071a1.927,1.927,0,0,0,2.645,0l2.645-2.535a1.74,1.74,0,0,0,0-2.535ZM25.983,28.466c8.2,0,14.847-6.372,14.847-14.233S34.183,0,25.983,0,11.135,6.372,11.135,14.233,17.782,28.466,25.983,28.466Zm10.393,3.558H34.438a20.984,20.984,0,0,1-16.912,0H15.59C6.983,32.025,0,38.719,0,46.97V51.6a5.458,5.458,0,0,0,5.568,5.337H46.4A5.458,5.458,0,0,0,51.965,51.6V46.97C51.965,38.719,44.982,32.025,36.376,32.025Z"
-                  fill="#f7b74a"
-                />
-              </svg>
-            </div>
-          </success-alert>
+			<div id="modal-area">
+				<transition>
+					<success-alert
+						description="Usuário removido com sucesso!"
+						v-if="showSuccessAlert"
+					>
+						<div class="text-center mb-2 mt-2" id="icon-1">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="55"
+								height="40"
+								viewBox="0 0 74.227 56.933"
+							>
+								<path
+									id="user-times-solid"
+									d="M68.39,26.687l5.289-5.071a1.74,1.74,0,0,0,0-2.535l-2.645-2.535a1.927,1.927,0,0,0-2.645,0L63.1,21.617l-5.289-5.071a1.927,1.927,0,0,0-2.645,0l-2.645,2.535a1.74,1.74,0,0,0,0,2.535l5.289,5.071-5.289,5.071a1.74,1.74,0,0,0,0,2.535l2.645,2.535a1.927,1.927,0,0,0,2.645,0L63.1,31.758l5.289,5.071a1.927,1.927,0,0,0,2.645,0l2.645-2.535a1.74,1.74,0,0,0,0-2.535ZM25.983,28.466c8.2,0,14.847-6.372,14.847-14.233S34.183,0,25.983,0,11.135,6.372,11.135,14.233,17.782,28.466,25.983,28.466Zm10.393,3.558H34.438a20.984,20.984,0,0,1-16.912,0H15.59C6.983,32.025,0,38.719,0,46.97V51.6a5.458,5.458,0,0,0,5.568,5.337H46.4A5.458,5.458,0,0,0,51.965,51.6V46.97C51.965,38.719,44.982,32.025,36.376,32.025Z"
+									fill="#f7b74a"
+								/>
+							</svg>
+						</div>
+					</success-alert>
 
-          <table-with-surplus
-            v-if="showTableWithSurplus"
-            key="table-with-surplus"
-          ></table-with-surplus>
+					<table-with-surplus
+						v-if="showTableWithSurplus"
+						key="table-with-surplus"
+					></table-with-surplus>
 
-          <modal-base v-if="showAlert">
-            <div id="alert-error">
-              <h1>AÇÃO NEGADA</h1>
-              <i class="fas fa-times-circle"></i>
-              <p>
-                Não é possível remover um usuário <br />
-                que possui saldo
-              </p>
-              <button @click="showAlert = false">OK</button>
-            </div>
-          </modal-base>
+					<modal-base v-if="showAlert">
+						<div id="alert-error">
+							<h1>AÇÃO NEGADA</h1>
+							<i class="fas fa-times-circle"></i>
+							<p>
+								Não é possível remover um usuário <br />
+								que possui saldo
+							</p>
+							<button @click="showAlert = false">OK</button>
+						</div>
+					</modal-base>
 
-          <modal v-if="showModal" action="removeUser" :reqBody="reqBody">
-            <div id="icon-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="55"
-                height="40"
-                viewBox="0 0 74.227 56.933"
-              >
-                <path
-                  id="user-times-solid"
-                  d="M68.39,26.687l5.289-5.071a1.74,1.74,0,0,0,0-2.535l-2.645-2.535a1.927,1.927,0,0,0-2.645,0L63.1,21.617l-5.289-5.071a1.927,1.927,0,0,0-2.645,0l-2.645,2.535a1.74,1.74,0,0,0,0,2.535l5.289,5.071-5.289,5.071a1.74,1.74,0,0,0,0,2.535l2.645,2.535a1.927,1.927,0,0,0,2.645,0L63.1,31.758l5.289,5.071a1.927,1.927,0,0,0,2.645,0l2.645-2.535a1.74,1.74,0,0,0,0-2.535ZM25.983,28.466c8.2,0,14.847-6.372,14.847-14.233S34.183,0,25.983,0,11.135,6.372,11.135,14.233,17.782,28.466,25.983,28.466Zm10.393,3.558H34.438a20.984,20.984,0,0,1-16.912,0H15.59C6.983,32.025,0,38.719,0,46.97V51.6a5.458,5.458,0,0,0,5.568,5.337H46.4A5.458,5.458,0,0,0,51.965,51.6V46.97C51.965,38.719,44.982,32.025,36.376,32.025Z"
-                  fill="#f7b74a"
-                />
-              </svg>
-            </div>
-          </modal>
-        </transition>
-      </div>
-      <!--modal are ends-->
-    </div>
-    <!--remove user ends-->
-  </transition>
+					<modal v-if="showModal" action="removeUser" :reqBody="reqBody">
+						<div id="icon-1">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="55"
+								height="40"
+								viewBox="0 0 74.227 56.933"
+							>
+								<path
+									id="user-times-solid"
+									d="M68.39,26.687l5.289-5.071a1.74,1.74,0,0,0,0-2.535l-2.645-2.535a1.927,1.927,0,0,0-2.645,0L63.1,21.617l-5.289-5.071a1.927,1.927,0,0,0-2.645,0l-2.645,2.535a1.74,1.74,0,0,0,0,2.535l5.289,5.071-5.289,5.071a1.74,1.74,0,0,0,0,2.535l2.645,2.535a1.927,1.927,0,0,0,2.645,0L63.1,31.758l5.289,5.071a1.927,1.927,0,0,0,2.645,0l2.645-2.535a1.74,1.74,0,0,0,0-2.535ZM25.983,28.466c8.2,0,14.847-6.372,14.847-14.233S34.183,0,25.983,0,11.135,6.372,11.135,14.233,17.782,28.466,25.983,28.466Zm10.393,3.558H34.438a20.984,20.984,0,0,1-16.912,0H15.59C6.983,32.025,0,38.719,0,46.97V51.6a5.458,5.458,0,0,0,5.568,5.337H46.4A5.458,5.458,0,0,0,51.965,51.6V46.97C51.965,38.719,44.982,32.025,36.376,32.025Z"
+									fill="#f7b74a"
+								/>
+							</svg>
+						</div>
+					</modal>
+				</transition>
+			</div>
+			<!--modal are ends-->
+		</div>
+		<!--remove user ends-->
+	</transition>
 </template>
 
 <script>
@@ -83,100 +83,100 @@ import TableWithUsers from "../components/TableWithUsers.vue";
 import TableWithSurplus from "../components/TableWithSurplus.vue";
 
 export default {
-  name: "RemoveUser",
-  data() {
-    return {
-      showModal: false,
-      showAlert: false,
-      showSuccessAlert: false,
-      showTableWithSurplus: false,
-      action: "remove",
-    };
-  },
-  computed: {
-    reqBody() {
-      return {
-        name: this.userInTable.name,
-        signUpDate: this.userInTable.signUpDate,
-        section: this.userInTable.section,
-      };
-    },
-    userInTable() {
-      return this.$store.state.userInActionTable;
-    },
-  },
-  created() {
-    EventBus.$on("openAlert", () => {
-      this.showAlert = true;
-    }),
-      EventBus.$on("openModal", () => {
-        this.showModal = true;
-      }),
-      EventBus.$on("closeModal", () => {
-        this.showModal = false;
-      }),
-      EventBus.$on("userRemoved", () => {
-        this.showModal = false;
-        this.showSuccessAlert = true;
-      });
-    EventBus.$on("closeSuccessAlert", () => {
-      this.showSuccessAlert = false;
-      this.$router.go();
-    });
-    EventBus.$on("openTableWithSurplus", () => {
-      this.showTableWithSurplus = true;
-    });
-    EventBus.$on("closeTableWithSurplus", () => {
-      this.showTableWithSurplus = false;
-    });
-  },
-  components: {
-    HeaderDefault,
-    Modal,
-    ModalBase,
-    SuccessAlert,
-    TableWithUsers,
-    TableWithSurplus,
-  },
+	name: "RemoveUser",
+	data() {
+		return {
+			showModal: false,
+			showAlert: false,
+			showSuccessAlert: false,
+			showTableWithSurplus: false,
+			action: "remove",
+		};
+	},
+	computed: {
+		reqBody() {
+			return {
+				name: this.userInTable.name,
+				signUpDate: this.userInTable.signUpDate,
+				section: this.userInTable.section,
+				email: this.userInTable.email,
+			};
+		},
+		userInTable() {
+			return this.$store.state.userInActionTable;
+		},
+	},
+	created() {
+		EventBus.$on("openAlert", () => {
+			this.showAlert = true;
+		}),
+			EventBus.$on("openModal", () => {
+				this.showModal = true;
+			}),
+			EventBus.$on("closeModal", () => {
+				this.showModal = false;
+			}),
+			EventBus.$on("userRemoved", () => {
+				this.showModal = false;
+				this.showSuccessAlert = true;
+			});
+		EventBus.$on("closeSuccessAlert", () => {
+			this.showSuccessAlert = false;
+			this.$router.go();
+		});
+		EventBus.$on("openTableWithSurplus", () => {
+			this.showTableWithSurplus = true;
+		});
+		EventBus.$on("closeTableWithSurplus", () => {
+			this.showTableWithSurplus = false;
+		});
+	},
+	components: {
+		HeaderDefault,
+		Modal,
+		ModalBase,
+		SuccessAlert,
+		TableWithUsers,
+		TableWithSurplus,
+	},
 };
 </script>
 
 <style scoped>
 * {
-  margin: 0;
-  padding: 0;
+	margin: 0;
+	padding: 0;
 }
 
 #remove-users,
 #modal-area {
-  width: 100%;
-  text-align: center;
-  display:flex;
-  flex-direction: column;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
+	width: 100%;
+	text-align: center;
+	display: flex;
+	flex-direction: column;
+	position: absolute;
+	top: 0;
+	left: 0;
+	right: 0;
 }
 
 #alert-error button:active {
-  background-color: #fcab1f;
+	background-color: #fcab1f;
 }
 
 .remove-user-enter-active,
 .remove-user-leave-active {
- 
-  transition: opacity 0.4s;
+	transition: opacity 0.4s;
 }
 
 .remove-user-enter,
 .remove-user-leave-to {
-  overflow: hidden;
-  opacity: 0;
+	overflow: hidden;
+	opacity: 0;
 }
 
 .remove-user-leave,
 .remove-user-enter-to {
-  opacity: 1;
+	opacity: 1;
 }
 </style>
